d700f53656f892db129caeee6239a3af1195d5dd
xuanco941 <xuanco941@gmail.com>
2022-06-08 17:36:24 +0700

    push

--- functions/controller/Chapter.js
+++ functions/controller/Chapter.js
@@ -1,14 +1,16 @@
 const { db } = require('../config/firebase');
+const { cloudinary } = require('../config/cloudinary');
+const fs = require('fs');
 
-class Chapter{
+class Chapter {
 
   // xem cac chapter
-  async GetAllChapters(req,res){
-    const {comicId} = req.query;
+  async GetAllChapters(req, res) {
+    const { comicId } = req.query;
     try {
       const allChapter = [];
       const querySnapshot = await db.collection('comics').doc(comicId).collection('chaps').get();
-      querySnapshot.forEach( (doc) => allChapter.push(doc.data()));
+      querySnapshot.forEach((doc) => allChapter.push(doc.data()));
       return res.status(200).json(allChapter);
     } catch (error) {
       return res.status(500).json(error.message);
@@ -16,8 +18,8 @@ class Chapter{
   }
 
   // xem 1 chapter
-  async GetAChapters(req,res){
-    const {comicId,tenChap} = req.query;
+  async GetAChapters(req, res) {
+    const { comicId, tenChap } = req.query;
     try {
       const querySnapshot = await (await db.collection('comics').doc(comicId).collection('chaps').doc(tenChap).get()).data();
       return res.status(200).json(querySnapshot);
@@ -27,27 +29,47 @@ class Chapter{
   }
 
 
-    //Thêm chapter
+  //Thêm chapter
   async AddChapter(req, res) {
     let tenChap = req.body.tenChap;
     let comicId = req.body.comicId;
-    console.log(req.files);
+    let tenTruyen = req.body.tenTruyen;
+
+    //up img to cloudinary and get link
+    let links = [];
+    if (req.files) {
+      for (const element of req.files) {
+        await cloudinary.uploader.upload('uploads/' + element.filename,
+          {
+            folder: `${tenTruyen}/${tenChap}`,
+            public_id: element.filename
+          },
+          function (error, result) {
+            links.push(result.url);
+            fs.unlinkSync('./uploads/' + element.filename);
+          });
+      }
+    }
+
+
+
     await db.collection('comics').doc(comicId).collection('chaps').doc(tenChap).set({
-      tenChap: tenChap
-        }).then(() => {
-        return res.status(200).json({
-            status: 'success',
-            message: 'Thêm chapter thành công.',
-            data: {
-                comicId,
-                tenChap
-            }
-          });
+      tenChap: tenChap,
+      links: links
+    }).then(() => {
+      return res.status(200).json({
+        status: 'success',
+        message: 'Thêm chapter thành công.',
+        data: {
+          comicId,
+          tenChap
+        }
+      });
     }).catch((error) => {
-        return res.status(400).json({
-            status: 'error',
-            message: error.message,
-          });
+      return res.status(400).json({
+        status: 'error',
+        message: error.message,
+      });
     })
   }
 
